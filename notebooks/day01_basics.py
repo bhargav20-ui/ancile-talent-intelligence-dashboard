76d08f9726e0b4ed85b5473583fa5c7b27224b27
Bhargav <nakkabhargav20@gmail.com>
2026-05-21 16:53:55 +0530

    Day 1: setup + two-sum

---
+++ notebooks/day01_basics.py
@@ -0,0 +1,6 @@
+n = [1, 2, 3, 4, 5]
+for i in n:	print(i)
+n1=["vinay","bhargav","venkata"] 
+for i,name in enumerate(n1): print(i,name)
+skills={"python":3,"SQL":2,"Excel":4} 
+for skill ,level in skills.items(): print(skill,"->",level)
