0cf61fc6608cb0fead8d2ab8ca838e2f9946fdbf
Bhargav <nakkabhargav20@gmail.com>
2026-05-22 16:10:30 +0530

    Week 1: dataset cleaned - nulls removed, skills standardized

--- notebooks/day01_basics.py
+++ notebooks/day01_basics.py
@@ -1,6 +1,16 @@
-n = [1, 2, 3, 4, 5]
-for i in n:	print(i)
-n1=["vinay","bhargav","venkata"] 
-for i,name in enumerate(n1): print(i,name)
-skills={"python":3,"SQL":2,"Excel":4} 
-for skill ,level in skills.items(): print(skill,"->",level)
+# # n = [1, 2, 3, 4, 5]
+# # for i in n:	print(i)
+# # n1=["vinay","bhargav","venkata"] 
+# # for i,name in enumerate(n1): print(i,name)
+# # skills={"python":3,"SQL":2,"Excel":4} 
+# # for skill ,level in skills.items(): print(skill,"->",level)
+
+# # LEETCODE TWO-SUM CODE:
+# class Solution:
+#     def twoSum(self, n: List[int], t: int) -> List[int]:
+#         for i in range(len(n)):
+#             for j in range(i+1,len(n)):
+#                 if (n[i]+n[j])==t:
+#                     return i,j
+
+import pandas as pd
